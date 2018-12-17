3af1da5a0491e50f91672217faa06e54e5ba1aed
Diego Paludo <diego.paludo@gmail.com>
2018-12-17 00:12:46 -0200

    userdata

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-apt -y full-upgrade
-apt install -y linux-image-4.18.0-0.bpo.1-amd64
+apt update
 apt install -y -t stretch-backports firmware-linux-nonfree firmware-iwlwifi
-apt install -y aptitude git python3-pip
-pip3 install ansible
+apt install -y aptitude git python3-pip apt-transport-https
+pip3 install ansible -U
+
+ssh-keyscan github.com > ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
 
 git clone git@github.com:diegopaludo/my_workstation.git
 
