41f9d54243c559cc83e0b10d03409af094243189
Diego Paludo <diego.paludo@rivendel.com.br>
2019-03-16 01:17:03 -0300

    install more pre req

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get update && apt-get -y full-upgrade
-apt-get -y install python3-pip apt-transport-https
+apt-get -y install python3-pip apt-transport-https dirmngr
 
 pip3 install ansible -U
 
